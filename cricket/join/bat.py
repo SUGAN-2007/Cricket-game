e4009164cbc13aae627cb90ca732a00a1f336316
SUGAN <developer.sugan@gmail.com>
2025-01-14 12:14:07 +0530

    try

--- cricket/join/bat.py
+++ cricket/join/bat.py
@@ -3,16 +3,21 @@ from join.over import n
 
 player_scores=0
 def batting():
-    player_score=0
-    for i in range(n[0]):
-        pc = random.randint(1,10)
-        run=int(input('enter the number b/w 1 t0 10 : '))
-        print(f'the bot number : {pc}')
-        if run != pc and run <=10:
-            player_score+=run
-        else:
-            print('you are out')
-            break 
-    global player_scores
-    player_scores+=player_score
-    print(f'the player score  : {player_score}')
+    try:
+
+        player_score=0
+        for i in range(n[0]):
+            pc = random.randint(1,10)
+            run=int(input('enter the number b/w 1 t0 10 : '))
+            print(f'the bot number : {pc}')
+            if run != pc and run <=10:
+                player_score+=run
+            else:
+                print('you are out')
+                break 
+        global player_scores
+        player_scores+=player_score
+        print(f'the player score  : {player_score}')
+    except ValueError:
+        print('enter the correct number')
+        batting()
--- cricket/join/bowl.py
+++ cricket/join/bowl.py
@@ -3,18 +3,22 @@ from join.over import n
 
 bot_scores=0
 def bowling():
-    bot_score=0
-    for i in range(n[0]):
-        pc = random.randint(1,10)
-        print('the bot number : **')
-        run=int(input('enter the number b/w 1 t0 10 : '))
-        if run != pc and run <=10:
-            print(f'the bot number : {pc}')
-            bot_score+=run
-        else:
-            print('bot is out')
-            break 
-    global bot_scores
-    bot_scores+=bot_score
-    print(f'the bot score : {bot_score}')
+    try:
+        bot_score=0
+        for i in range(n[0]):
+            pc = random.randint(1,10)
+            print('the bot number : **')
+            run=int(input('enter the number b/w 1 t0 10 : '))
+            if run != pc and run <=10:
+                print(f'the bot number : {pc}')
+                bot_score+=run
+            else:
+                print('bot is out')
+                break 
+        global bot_scores
+        bot_scores+=bot_score
+        print(f'the bot score : {bot_score}')
+    except valueError:
+        print('enter the correct number')
+        bowling()
 
