33b5977940656e13e9dc6501d5a21d5b6ebc6399
SUGAN <developer.sugan@gmail.com>
2025-03-02 19:33:43 +0530

    ...

--- cricket/join/dec.py
+++ cricket/join/dec.py
@@ -5,15 +5,15 @@ def decs(x,y):
     b=['batting','bowling']
     f=''.join(random.choices(b))
     b=['batting','bat']
-    bo=['bowling','bowl']
+    bo=['bowling','bowl'] 
     if x == y:
         d=input(' \n\nBATTING or BOWLING :').lower()      
         if d in b:
             print('')
-            print('you choose to bat first')
+            print('you choose to bat first') 
             batting() 
             print("\n\n your's bowling turn")
-            bowling()
+            bowling() 
         elif d in bo:
             print('')
             print('you choose to bowl first')
@@ -21,21 +21,21 @@ def decs(x,y):
             print("\n\n your's batting turn")
             batting()
             
-        else:
-            decs(x,y) 
+        else: 
+            decs(x,y)   
     else:
         print(" ")
         print('bot choose to :',f)
         if f == 'batting':
             print('')
             print('bot choose to bat first')
-            bowling()
+            bowling() 
             print("\n\n your's batting turn")
             batting()
 
         else:
             print('')
-            print('bot choose to bowl first')
+            print('bot choose to bowl first') 
             batting()
             print("\n\n your's bowling turn")
             bowling()
